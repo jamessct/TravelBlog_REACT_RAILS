c5ef5783dffd5fd947a233fc8a66575288816ee1
James Scott <jamessct@hotmail.co.uk>
2016-11-07 11:28:07 +0000

    updated the seed file

--- rails/scrappiii/db/seeds.rb
+++ rails/scrappiii/db/seeds.rb
@@ -11,11 +11,11 @@ Photo.delete_all()
 Entry.delete_all()
 Project.delete_all()
 
-p1 = Project.create({title: "florida holiday", summary: "a delightful family trip to the disney world", author: "James Scott"})
+p1 = Project.create({title: "Florida Holiday", summary: "A delightful family trip to Disney World", author: "James Scott"})
 
-e1 = Entry.create({title: "Day 1", tagline: "fun fun fun in the sun sun sun", mainPhoto: "http://www.britishairways.com/assets/images/global/components/bodyCopy/240/240x240-florida-sign-getty-157557967_75.jpg", body: "Today we went on holiday to florida. bla bla bla bla bla bla bla bla bla bla bla bla", project_id: p1.id})
+e1 = Entry.create({title: "Day 1", tagline: "What a long plane journey", mainPhoto: "http://www.britishairways.com/assets/images/global/components/bodyCopy/240/240x240-florida-sign-getty-157557967_75.jpg", body: "Today we went on holiday to florida.  What a long plane journey that was.  After driving to our spacious poolside appartment, we all headed for a snooze to combat the jetlag.", project_id: p1.id})
 
-e2 = Entry.create({title: "Day 2", tagline: "too many crocs", mainPhoto: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Nile_crocodile_head.jpg", body: "there are crocodiles all over the shop. they are quite spooky", project_id: p1.id})
+e2 = Entry.create({title: "Day 2", tagline: "Epcot Center..... more like Epic Center", mainPhoto: "https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/90/wdpromedia.disney.go.com/media/wdpro-assets/gallery/destinations/epcot/epcot-gallery-00.jpg?01052013114909", body: "Today we went to Epcot Center to see the giant golf ball.  It looked incredible from the monorail.", project_id: p1.id})
 
 ph1 = Photo.create({title: "Stock photo", link: "https://i.ytimg.com/vi/Y0Ni4S8HuJg/maxresdefault.jpg", description: "spooky crocodile", entry_id: e1.id})
 
@@ -33,5 +33,6 @@ ph7 = Photo.create({title: "Stock photo", link: "http://www.livescience.com/imag
 
 ph8 = Photo.create({title: "Stock photo", link: "http://www.livescience.com/images/i/000/016/951/original/alligator-smiles.JPG?interpolation=lanczos-none&downsize=660:*", description: "spooky crocodile 2", entry_id: e2.id})
 
-c1 = Comment.create({comment: "needs expanding", author: "James Scott", entry_id: e1.id})
-c2 = Comment.create({comment: "your shit mate ha ha", author: "big beano", entry_id: e1.id})
+c1 = Comment.create({comment: "Looks super fun, I'm so jealous", author: "Gran", entry_id: e1.id})
+c2 = Comment.create({comment: "Wish you were here gran!!!!", author: "James", entry_id: e1.id})
+c3 = Comment.create({comment: "Wow, that is a fairly sizeable ball.", author: "Big Beano", entry_id: e2.id})
